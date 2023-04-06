77f0fe62c0897cf03a9caede91a9c892a5c6c422
mikim <mikimeerson44@gmail.com>
2023-04-06 16:24:17 +0300

    change server to python flask framework

---
+++ dl-final-project-server/src/routes.py
@@ -0,0 +1,18 @@
+from .controller import upload, getListFiles, download, remove
+
+def routes(app):
+    @app.route('/upload', methods=['POST'])
+    def upload_route():
+        return upload()
+
+    @app.route('/files', methods=['GET'])
+    def getListFiles_route():
+        return getListFiles()
+
+    @app.route('/files/<string:name>', methods=['GET'])
+    def download_route(name):
+        return download(name)
+
+    @app.route('/files/<string:name>', methods=['DELETE'])
+    def remove_route(name):
+        return remove(name)
