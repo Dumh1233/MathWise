8ed845060930ad06f9c78b424821240c543aa3c1
mikim <mikimeerson44@gmail.com>
2023-05-25 13:04:08 +0300

    merge

--- dl-final-project-client/src/components/file-results.component.tsx
+++ dl-final-project-client/src/components/file-results.component.tsx
@@ -5,12 +5,15 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import '../styles/file-results.css';
 
+interface Equation {
+    image: string;
+    result: string;
+}
+
 const FileResults = () => {
     const location = useLocation()
     const { from } = location.state
     const nav = useNavigate();
-    console.log(from.segmentedEquations.length);
-    console.log(new Set(from.segmentedEquations).size);
     return (
         <>
             <button className="btn btn-primary backButton" type="button" onClick={() => nav(-1)}>
@@ -18,12 +21,12 @@ const FileResults = () => {
             </button>
             <h1 className="filename">{from.name.split('.')[0]}</h1>
             <div>
-                {from.segmentedEquations && from.segmentedEquations.map((url:string, key:string)=> (
+                {from.segmentedEquations && from.segmentedEquations.map((equation:Equation, key:string)=> (
                     <div className="imageContainer" key={key}>
                         <img 
                             alt="segmented page" 
                             className="img-fluid"
-                            src={url} 
+                            src={equation.image} 
                         />
                         <br />
                     </div>
