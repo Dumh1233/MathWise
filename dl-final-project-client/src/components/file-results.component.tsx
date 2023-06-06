2c7b07efc38dfff503be18e578570aaa7818d59a
Miki Meerson <mikimeerson44@gmail.com>
2023-06-06 07:52:21 +0300

    add table

--- dl-final-project-client/src/components/file-results.component.tsx
+++ dl-final-project-client/src/components/file-results.component.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom'
 import { useNavigate } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faChevronDown, faCircleXmark, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import '../styles/file-results.css';
 
 interface Question {
@@ -23,103 +23,40 @@ const FileResults = ({ fileInfos }: Props) => {
 
     const fileInfo = fileInfos.find((fileInfo) => fileInfo.name === filename);
 
-    const [equations, setEquations] = useState<any[]>([]);
-    const [shapes, setShapes] = useState<any[]>([]);
-    const [shapeMultiples, setShapeMultiples] = useState<any[]>([]);
-
-    type question_types_dict_type = {'equation': (param: any) => void, 'shape': (param: any) => void, 'shapeMultiple': (param: any) => void};
-    const QUESTION_TYPES_DICT: question_types_dict_type = {'equation': setEquations, 'shape': setShapes, 'shapeMultiple': setShapeMultiples};
-
-    useEffect(() => {
-        Object.keys(QUESTION_TYPES_DICT).forEach((questionType: string) => {
-            let questions = fileInfo.questionsData.filter((quesion: any) => quesion.type === questionType);
-            QUESTION_TYPES_DICT[questionType as keyof question_types_dict_type](questions);
-        })            
-    }, []);
-
     return (
         <>
             <button className="btn btn-primary backButton" type="button" onClick={() => nav(-1)}>
                 <FontAwesomeIcon icon={faArrowLeft} />
             </button>
             <h1 className="filename">{fileInfo.name.split('.')[0]}</h1>
-            <div className="accordion accordion-flush" id="questionsContainer">
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="equationHeading">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionEquations">
-                        Equations ({equations.length})
-                        <FontAwesomeIcon icon={faChevronDown}/>
-                    </button>
-                    </h2>
-                    <div id="accordionEquations" className="accordion-collapse collapse" data-bs-parent="#questionsContainer">
-                        <div className="accordion-body">
-                            {equations &&
-                                equations.map((q:Question, index:number)=> (
-                                    <div className="questionContainer" key={index}>
-                                        <img 
-                                            alt="segmented page" 
-                                            className="img-fluid"
-                                            src={q.image} 
-                                        />
-                                        <div>{q.parsed}</div>
-                                        {q.result ? <div>{q.result}</div> : <div>None</div>}
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="shapesHeading">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionShapes">
-                        Shapes ({shapes.length})
-                        <FontAwesomeIcon icon={faChevronDown}/>
-                    </button>
-                    </h2>
-                    <div id="accordionShapes" className="accordion-collapse collapse" data-bs-parent="#questionsContainer">
-                        <div className="accordion-body">
-                            {shapes &&
-                                shapes.map((q:Question, index:number)=> (
-                                    <div className="questionContainer" key={index}>
-                                        <img 
-                                            alt="segmented page" 
-                                            className="img-fluid"
-                                            src={q.image} 
-                                        />
-                                        <div>{q.parsed}</div>
-                                        {q.result ? <div>{q.result}</div> : <div>None</div>}
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-                <div className="accordion-item">
-                    <h2 className="accordion-header" id="shapeMultiplesHeading">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#accordionShapeMultiples">
-                        shapeMultiples ({shapeMultiples.length})
-                        <FontAwesomeIcon icon={faChevronDown}/>
-                    </button>
-                    </h2>
-                    <div id="accordionShapeMultiples" className="accordion-collapse collapse" data-bs-parent="#questionsContainer">
-                        <div className="accordion-body">
-                            {shapeMultiples &&
-                                shapeMultiples.map((q:Question, index:number)=> (
-                                    <div className="questionContainer" key={index}>
-                                        <img 
-                                            alt="segmented page" 
-                                            className="img-fluid"
-                                            src={q.image} 
-                                        />
-                                        <div>{q.parsed}</div>
-                                        {q.result ? <div>{q.result}</div> : <div>None</div>}
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Detected</th>
+                        <th scope="col">Parsed</th>
+                        <th scope="col">Result</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {fileInfo.questionsData.map((q:Question, index:number)=> (
+                        <tr key={index}>
+                            <th scope="row">
+                                <img 
+                                    alt="segmented page" 
+                                    className="img-fluid"
+                                    src={q.image} 
+                                />
+                            </th>
+                            <th scope="row">{q.parsed}</th>
+                            <th scope="row">
+                                {q.result &&  q.result == "Correct" ? <FontAwesomeIcon icon={faCheckCircle} style={{ color: 'green', fontSize: '30px' }}/> : 
+                                    (q.result == "Wrong" ? <FontAwesomeIcon icon={faCircleXmark} style={{ color: 'red', fontSize: '30px' }}/> : <span>None</span>)
+                                }
+                            </th>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
