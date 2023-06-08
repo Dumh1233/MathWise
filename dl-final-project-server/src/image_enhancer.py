4398781ca639ba7d8e599571adae3d0ce9bbf306
orel <322293473@idf.il>
2023-06-08 20:10:25 +0300

    update

--- dl-final-project-server/src/image_enhancer.py
+++ dl-final-project-server/src/image_enhancer.py
@@ -1,16 +1,18 @@
 import requests
 
+API_KEY = 'wx9gx3dpqb8f82arx'
 
-def get_image_enhancer(image_path): 
-  response = requests.request(
-    "POST", 
-    "https://techhk.aoscdn.com/api/tasks/visual/scale", 
-    headers= {'X-API-KEY': 'wxgryrq9493h6m8hz'}, 
-    data={'sync': '1','type':'face'}, 
-    files= {
-    'image_file': open(image_path, 'rb')
-  })
-  return eval(response.text)
+
+def get_image_enhancer(image_path):
+    response = requests.request(
+        "POST",
+        "https://techhk.aoscdn.com/api/tasks/visual/scale",
+        headers={'X-API-KEY': API_KEY},
+        data={'sync': '1', 'type': 'face'},
+        files={
+            'image_file': open(image_path, 'rb')
+        })
+    return eval(response.text)
 
 
 def image_enhancer(image_path):
